8ad23c877f50af3a5f3296a3154b94427e68309e
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-27 19:40:58 +0100

    + sync.ps1

---
+++ sync.ps1
@@ -0,0 +1,32 @@
+git checkout master
+
+if (! $?) {
+   exit
+}
+
+git pull https://github.com/vim/vim
+
+if (! $?) {
+   exit
+}
+
+git push https://github.com/ReneNyffenegger/vim master
+
+if (! $?) {
+   exit
+}
+
+git checkout tq84
+
+if (! $?) {
+   exit
+}
+
+git merge master
+
+if (! $?) {
+   exit
+}
+
+git push https://github.com/ReneNyffenegger/vim tq84
+
